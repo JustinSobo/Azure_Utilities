01a3af023b2d48421ae26027bec793f766e9a41c
Justin Stevens <37387224+JustinSobo@users.noreply.github.com>
2021-03-13 13:21:33 -0500

    Create Connect-AzureRmAccount.ps1

---
+++ Connect-AzureRmAccount.ps1
@@ -0,0 +1,4 @@
+# The Connect-AzureRmAccount cmdlet connects to Azure with an authenticated account for use with Azure Resource Manager cmdlet requests.
+# After executing this cmdlet, you can disconnect from an Azure account using Disconnect-AzureRmAccount.
+
+Connect-AzureRmAccount
